66e6f9b1a14208de85735899aca3b8fda259e260
StiwardSolano <spanker3@gmail.com>
2020-01-19 22:58:59 -0500

    Initial commit

---
+++ client/build/precache-manifest.cbf491947a4323b9897f567f4d6e01f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5f3579ffdb238746ef92834ba6b7fb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64a0ded1298319c96aef",
+    "url": "/static/css/2.89d568a6.chunk.css"
+  },
+  {
+    "revision": "64a0ded1298319c96aef",
+    "url": "/static/js/2.d1b89373.chunk.js"
+  },
+  {
+    "revision": "c0a4706aa6c9bd8b37d9",
+    "url": "/static/js/main.07ef3765.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
