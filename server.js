930aecfef770f204531987fa73d583f9061660fd
StiwardSolano <spanker3@gmail.com>
2020-04-05 17:08:14 -0500

    update deps

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
+const uri = process.env.ATLAS_URI || 'mongodb+srv://usertst:usertst@cluster0-mwkmh.mongodb.net/test?retryWrites=true&w=majority';
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
 );
 const connection = mongoose.connection;
